4075bf878ad475d8b5b37cb53625713a0a832106
Ayorinde Adunse <adunseayorinde@gmail.com>
2020-07-07 19:09:43 +0100

    updated nav-bar component and google auth

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from "@angular/core";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { Component } from "@angular/core";
+import * as firebase from "firebase";
 
 @Component({
   selector: "bs-navbar",
   templateUrl: "./bs-navbar.component.html",
   styleUrls: ["./bs-navbar.component.css"],
 })
-export class BsNavbarComponent implements OnInit {
-  constructor() {}
+export class BsNavbarComponent {
+  user: firebase.User;
+  constructor(private afAuth: AngularFireAuth) {
+    // authstate is an Observable which represents the authentication
+    // state of the current user, so we can just subscribe to it.
 
-  ngOnInit(): void {}
+    afAuth.authState.subscribe((user) => (this.user = user));
+  }
+
+  logout() {
+    this.afAuth.signOut();
+  }
 }
