785c8d7d07576183c473713c0c74d68930dc3af1
Ayorinde Adunse <adunseayorinde@gmail.com>
2020-07-08 09:27:06 +0100

    upd. authguard service to fix redirect after login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthService } from "./auth.service";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'sativa-ngr';
+  //title = 'sativa-ngr';
+  constructor(private auth: AuthService, router: Router) {
+    auth.user$.subscribe((user) => {
+      if (user) {
+        let returnUrl = localStorage.getItem("returnUrl");
+        router.navigateByUrl(returnUrl);
+      }
+    });
+  }
 }
